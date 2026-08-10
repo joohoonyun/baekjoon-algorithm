4ccfd031c28e0799e22920ca2a12bfc02ff4a0a6
joohoon.dev <121741140+joohoonyun@users.noreply.github.com>
2026-08-10 22:07:00 +0900

    Time: 1 ms (33.33%), Space: 41.1 MB (79.21%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.kt
+++ 0075-sort-colors/0075-sort-colors.kt
@@ -1,23 +1,24 @@
 class Solution {
     fun sortColors(nums: IntArray): Unit {
-        quickSort(0, nums.size-1, nums)     
+        quickSort(0, nums.size-1, nums)      
     }
 
     fun quickSort(start: Int, end: Int, nums: IntArray): Unit {
         if (start >= end) return
-        val pivotIdx = partition(start, end, nums)
+        val pivotIdx = getPartition(start, end, nums)
         
         quickSort(start, pivotIdx-1, nums)
         quickSort(pivotIdx+1, end, nums)
     }
 
-    fun partition(start: Int, end: Int, nums: IntArray): Int {
+    fun getPartition(start: Int, end: Int, nums: IntArray): Int {
         val pivot = nums[end]
         var smallerIdx = start
+
         for (curIdx: Int in start until end) {
             if (pivot >= nums[curIdx]) {
                 swap(curIdx, smallerIdx, nums)
-                smallerIdx++        
+                smallerIdx++
             }
         }
 
@@ -25,9 +26,9 @@ class Solution {
         return smallerIdx
     }
 
-    fun swap(a: Int, b: Int, target: IntArray) {
-        var temp = target[a]
-        target[a] = target[b]
-        target[b] = temp
+    fun swap(a: Int, b: Int, nums: IntArray): Unit {
+        var temp = nums[a]
+        nums[a] = nums[b]
+        nums[b] = temp
     }
 }
