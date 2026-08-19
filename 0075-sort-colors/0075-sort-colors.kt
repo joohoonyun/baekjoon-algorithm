a176f5b45a9ce76285ad1313f90456f43fe0ab35
joohoon.dev <121741140+joohoonyun@users.noreply.github.com>
2026-08-19 21:41:49 +0900

    Time: 0 ms (100%), Space: 41.3 MB (62.55%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.kt
+++ 0075-sort-colors/0075-sort-colors.kt
@@ -1,33 +1,31 @@
 class Solution {
     fun sortColors(nums: IntArray): Unit {
-        quickSort(0, nums.size-1, nums)   
+        quickSort(0, nums.size-1, nums)
     }
 
     fun quickSort(start: Int, end: Int, nums: IntArray): Unit {
-        if (start >= end) return
-        val pivotIdx: Int = getPartition(start, end, nums)
-        
-        quickSort(start, pivotIdx-1, nums)
-        quickSort(pivotIdx+1, end, nums)
+        if (start > end) return
+        val pivot: Int = getPartition(start, end, nums)
+
+        quickSort(start, pivot-1, nums)
+        quickSort(pivot+1, end, nums)
     }
 
     fun getPartition(start: Int, end: Int, nums: IntArray): Int {
-        val pivot: Int = nums[end]
-        var smallerIdx = start
-
+        val pivot = nums[end]
+        var smallerIdx: Int = start
         for (curIdx: Int in start until end) {
             if (nums[curIdx] <= pivot) {
-                swap(smallerIdx, curIdx, nums)
-                smallerIdx++;
+                if (curIdx != smallerIdx) swap(smallerIdx, curIdx, nums)
+                smallerIdx++
             }
         }
-
         swap(smallerIdx, end, nums)
         return smallerIdx
     }
 
     fun swap(a: Int, b: Int, nums: IntArray) {
-        var temp: Int = nums[a]
+        var temp = nums[a]
         nums[a] = nums[b]
         nums[b] = temp
     }
