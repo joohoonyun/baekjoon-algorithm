22f54528aac82799f60f026e34c46db19fbfddaf
joohoon.dev <121741140+joohoonyun@users.noreply.github.com>
2026-08-22 21:31:18 +0900

    Time: 17 ms (42.35%), Space: 47.2 MB (95.34%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,39 +1,41 @@
 class Solution {
-    int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
-    int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+    int[] dx = new int[]{0, -1, 1, 0, 1, -1, -1, 1};
+    int[] dy = new int[]{1, 0, 0, -1, 1, -1, 1, -1}; // 대각선까지
+
     public int shortestPathBinaryMatrix(int[][] grid) {
         int x = grid[0].length-1;
         int y = grid.length-1;
 
-        if (grid[0][0] == 1 || grid[y][x] == 1) {
+        if (grid[x][y] == 1 || grid[0][0] == 1) { // 목적지가 1이면 어차피 갈 수 없다.
             return -1;
         }
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0, 0, 1}); // 0, 0부터 시작
-        int[][] visited = new int[x+1][y+1]; // x, y 가 배열의 길이인지 체크해야함
-        visited[0][0] = 1;
-        
-        while (!q.isEmpty()) {
-            int[] popped = q.poll();
-            int px = popped[0];
-            int py = popped[1];
-            int distance = popped[2];
 
-            if (px == x && py == y) {
+        Queue<int[]> q = new ArrayDeque<>();
+        boolean[][] visited = new boolean[x+1][y+1];
+        q.add(new int[]{0,0,1});
+        visited[0][0] = true;
+        
+
+        while (!q.isEmpty()) {
+            int[] polled = q.poll();
+            int cx = polled[0];
+            int cy = polled[1];
+            int distance = polled[2];
+
+            if (cx == x && cy == y) { // 방문한 위치가 목적지라면 거리를 return
                 return distance;
             }
-            
-            // 8방향 위, 아래, 왼, 오, 대각선 4방향
+
             for (int i=0; i<8; ++i) {
-                int nx = px + dx[i];
-                int ny = py + dy[i];
-                // 밖으로 벗어나는 영역이면 pass
+                int nx = cx + dx[i]; // 방문예정 x축
+                int ny = cy + dy[i]; // 방문예정 y축
+
                 if (nx > x || ny > y || nx < 0 || ny < 0) continue;
-                if (visited[nx][ny] == 1) continue;
-                if (grid[nx][ny] == 1) continue; 
-                
+                if (visited[nx][ny]) continue;
+                if (grid[nx][ny] == 1) continue; // 1은 못감
+
                 q.add(new int[]{nx, ny, distance+1});
-                visited[nx][ny] = 1;
+                visited[nx][ny] = true;
             }
         }
         return -1;
