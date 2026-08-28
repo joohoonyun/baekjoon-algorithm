eea0b1c891412e0bf53b0679b5fd1b3f1117961e
joohoon.dev <121741140+joohoonyun@users.noreply.github.com>
2026-08-28 22:26:02 +0900

    [level 2] Title: 큰 수 만들기, Time: 21.54 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -2,26 +2,24 @@ import java.util.*;
 
 class Solution {
     public String solution(String number, int k) {
-        // 탐욕법
-        // 최적의 값을 구해야한다.
-        // 현재 숫자가 이전 숫자보다 크면 이전숫자를 지운다.
-        Stack<Character> stack = new Stack<>();
-        stack.push(number.charAt(0));
-        for (int i=1; i<number.length(); ++i) {
-            while (!stack.isEmpty() && k > 0 && stack.peek() < number.charAt(i)) {
-                stack.pop();
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char now : number.toCharArray()) {
+            while (!stack.isEmpty() && k > 0 && stack.peekLast() < now) {
+                stack.pollLast();
                 k--;
             }
-            stack.push(number.charAt(i));
+
+            stack.addLast(now);
         }
-        
+
         while (k > 0) {
-            stack.pop();
+            stack.pollLast();
             k--;
         }
-        
-        // 스택의 모든 문자 합치기
+
         StringBuilder sb = new StringBuilder();
+
         for (char c : stack) {
             sb.append(c);
         }
