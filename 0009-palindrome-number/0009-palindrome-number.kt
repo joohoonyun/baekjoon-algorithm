97b64793d6118f7ecffc66fa637b405f4f3ce249
joohoon.dev <121741140+joohoonyun@users.noreply.github.com>
2026-09-02 15:26:28 +0900

    Time: 5 ms (99.28%), Space: 45.9 MB (51.44%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.kt
+++ 0009-palindrome-number/0009-palindrome-number.kt
@@ -1,14 +1,22 @@
 class Solution {
     fun isPalindrome(x: Int): Boolean {
-        if (x < 0 || (x % 10 == 0 && x != 0)) return false
-        var number = x
-        var revertedNumber: Int = 0
-
-        while (number > revertedNumber) {
-            revertedNumber = revertedNumber * 10 + number % 10 // 1
-            number /= 10 // 12
+        // x / 10 을 하면 맨 오른쪽 수를 제거할 수 있다.
+        // x % 10 을 하면 맨 오른쪽 수를 추출할 수 있다.
+        // 음수는 palindrome이 될 수 없음
+        // 0이 아닌 숫자가 0으로 끝나면 palindrome이 될 수 없음
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false
         }
 
-        return revertedNumber == number || number == revertedNumber/10
+        var num = x
+        var revertedNumber = 0
+
+        // 154321
+        while (revertedNumber < num) {
+            revertedNumber = revertedNumber * 10 + num % 10
+            num /= 10
+        }
+
+        return num == revertedNumber || (num == revertedNumber/10)
     }
 }
